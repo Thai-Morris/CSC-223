f34429fbd6ad58449842eb13b135b6d17bba06ea
Thai Morris <30935442+Thai-Morris@users.noreply.github.com>
2017-12-02 18:35:54 -0500

    done the selceting elements
    Now, I will do the Scripted Product List

--- select.js
+++ select.js
@@ -1,6 +1,11 @@
-var filterButton = document.getElementById("filter"); 
-
-filterButton.addeventListener("click", fuction() {
-var Listings = document.getElementByClassName("favorite-item");
+var myButton = document.getElementById("filter"); 
+myButton.onclick = function() {
+var Listings = document.getElementsByClassName("favorite-item");
 Listings[0].parentNode.removeChild(Listings[0]);
-});
+};
+
+var Highlight = document.getElementById("highlight"); 
+Highlight.onclick = function() {
+var Yellow = document.getElementsByClassName("favorite-item");
+Yellow[0].style.color = "red";
+};
